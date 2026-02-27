65f08698c0eae28196b37addd734b79ea87acc93
piyush8512 <prajapatipiyush851@gmail.com>
2026-02-27 15:25:41 +0530

    feat: dashboard uimaded

---
+++ frontend/app/(main)/layout.tsx
@@ -0,0 +1,5 @@
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="pt-10">{children}</div>;
+};
+
+export default MainLayout;
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    domains: ["www.themealdb.com", "images.unsplash.com", "source.unsplash.com",],
+
+  },
 };
 
 export default nextConfig;
